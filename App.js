1009b4a168acc2c6251a9f7df5fdc859073e0674
idris agboola <agboolaisholaidreez@gmail.com>
2021-04-17 14:05:00 +0100

    update

--- App.js
+++ App.js
@@ -1,59 +1,12 @@
-import React, { useState } from "react";
-import {
-  Button,
-  ScrollView,
-  StyleSheet,
-  Text,
-  TextInput,
-  View,
-} from "react-native";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Header from "./components/header";
 
 export default function App() {
-  const [person, setperson] = useState([
-    {
-      name: "Idris",
-    },
-    {
-      name: "Ishola",
-    },
-    {
-      name: "Agboola",
-    },
-    {
-      name: "Hammed",
-    },
-    {
-      name: "Bashiru",
-    },
-    {
-      name: "Basit",
-    },
-    {
-      name: "Afeez",
-    },
-    {
-      name: "Asabi",
-    },
-    {
-      name: "Asabi",
-    },
-    {
-      name: "Asabi",
-    },
-    {
-      name: "Asabi",
-    },
-  ]);
-
   return (
     <View style={styles.container}>
-      <ScrollView>
-        {person.map((name, index) => (
-          <View key={index}>
-            <Text style={styles.name}>{name.name}</Text>
-          </View>
-        ))}
-      </ScrollView>
+      <Header />
+      <Text>Hello</Text>
     </View>
   );
 }
@@ -61,14 +14,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+
     // alignItems: "center",
     //justifyContent: "center",
-  },
-  name: {
-    backgroundColor: "pink",
-    padding: 30,
-    marginTop: 20,
-    marginHorizontal: 10,
-    fontSize: 20,
   },
 });
---
+++ components/header.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
+function Header() {
+  return (
+    <View style={styles.header}>
+      <Text style={styles.logo}>Todo App</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  header: {
+    backgroundColor: "coral",
+    paddingTop: 30,
+    height: 90,
+  },
+  logo: {
+    textAlign: "center",
+    color: "#f7f7f7",
+    fontWeight: "bold",
+    fontSize: 20,
+    marginTop: 20,
+  },
+});
+
+export default Header;
