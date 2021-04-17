24edba4ae27eab52411934297c02c710cc7efdc1
idris agboola <agboolaisholaidreez@gmail.com>
2021-04-17 09:24:46 +0100

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.header}>
+        <Text>HELLO REACT-NATIVE</Text>
+      </View>
+      <View style={styles.body}>
+        <Text>
+          <Text>lorem palem anti popo adsf.</Text>
+          <Text>lorem palem anti popo adsf.</Text>
+          <Text>lorem palem anti popo adsf.</Text>
+        </Text>
+        <Text>lorem palem anti popo adsf.</Text>
+        <Text>lorem palem anti popo adsf.</Text>
+      </View>
     </View>
   );
 }
@@ -14,8 +23,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#090",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  header: {
+    backgroundColor: "pink",
+    padding: 20,
+  },
+  text: {
+    color: "yellow",
   },
 });
